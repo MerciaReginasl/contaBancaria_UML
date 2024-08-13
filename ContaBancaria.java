1dffd8dabc445226cc691c7c8d359facb4b8d587
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2024-08-13 10:43:32 -0300

    Add files via upload

---
+++ ContaBancaria.java
@@ -0,0 +1,39 @@
+package app;
+
+public class ContaBancaria {
+
+    private string nomeProprietario;
+
+    private int numero;
+
+    private float saldo;
+
+    public ContaBancaria(string nomeProprietario) {
+
+    }
+
+    public void sacar(float valor) {
+
+    }
+
+    public void depositar(float valor) {
+
+    }
+
+    public string visualizar() {
+
+    }
+
+    public string getNomeProprietario() {
+
+    }
+
+    public int getNumero() {
+
+    }
+
+    public float getSaldo() {
+
+    }
+
+}
